1495e2dd75ed98cf4175e0ef28b807ed7a1b1506
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2025-02-27 20:25:41 +0600

    price box added

--- js/script.js
+++ js/script.js
@@ -5,8 +5,7 @@ for(let i = 0; i < ringButtons.length; i++) {
     ringBtn.addEventListener("click", function(event) {
         const color = event.target.id.replace("-color", "");
         console.log(color)
-        
-        console.log(color);
+
         for(let j = 0; j < ringButtons.length; j++) {
             ringButtons[j].classList.remove("border-purple-500");
             ringButtons[i].classList.add("border-gray-100");
@@ -16,8 +15,25 @@ for(let i = 0; i < ringButtons.length; i++) {
         event.target.classList.remove("border-gray-100");
 
         const productImage = document.getElementById("product-image");
-        productImage.src = '../images/teal.png';
+        // productImage.src = '../images/teal.png';
+        // productImage.src = "../images/" + color + ".png";
+        productImage.src = `../images/${color}.png`;
     });
-
 }
 
+function selectWristSize(size) {
+    const sizes = ["S", "M", "L", "XL"];
+
+    for(let i = 0; i < sizes.length; i++){
+        const button = document.getElementById("size-" + sizes[i]);
+        // console.log(button)
+        const element = sizes[i];
+        if(size === element) {
+            button.classList.add("border-purple-600");
+            button.classList.add("bg-purple-100");
+        } else {
+            button.classList.remove("border-purple-600");
+            button.classList.remove("bg-purple-100");
+        }
+    }
+}
